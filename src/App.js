c008485eb21427971d435ee23750f41b380a110f
Yaswanth Tondepu <yaswanthtondepu@gmail.com>
2023-01-13 13:05:56 -0600

    register page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './components/HomePage';
 import CreatePost from './components/CreatePost';
 import PostContent from './components/PostContainer';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/createpost" element={<CreatePost />} />
         <Route path="/post/:postId" element={<PostContent />} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </BrowserRouter>
 
